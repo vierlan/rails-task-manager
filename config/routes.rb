764f5cfc751c6ac69ca28ca571c117a4cc6eb852
Lananh Nguyen <lananhnguyen@live.co.uk>
2024-05-01 00:08:44 +0100

    task list working

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,12 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root 'tasks#index'
+  get 'tasks', to: 'task#index'
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  patch 'tasks/:id', to: 'tasks#update'
+  get 'tasks/:id/edit', to: 'tasks#edit'
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
